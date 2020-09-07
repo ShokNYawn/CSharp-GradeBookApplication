7add8337b62573e32905cb6536afbc4a30a9cf88
Griffith <smgriffith19@gmail.com>
2020-09-06 19:25:23 -0500

    Fix

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -40,26 +40,23 @@ namespace GradeBook.UserInterfaces
                 return;
             }
             var name = parts[1];
-            var value = parts[2];
+            var type = parts[2].ToLower();
 
-            if (value == "standard")
-            {
-                StandardGradeBook gradeBook = new StandardGradeBook(name);
-                GradeBookUserInterface.CommandLoop(gradeBook);
-            }
-            else if (value == "ranked")
-            {
-                RankedGradeBook gradeBook = new RankedGradeBook(name);
-                GradeBookUserInterface.CommandLoop(gradeBook);
-            }
+            BaseGradeBook gradeBook;
+
+            if (type == "standard")
+                gradeBook = new StandardGradeBook(name);
+            else if (type == "ranked")
+                gradeBook = new RankedGradeBook(name);
+
             else
             {
-                Console.WriteLine(" {name} is not a supported type of gradebook, please try again");
+                Console.WriteLine(" {0} is not a supported type of gradebook, please try again", type);
                 return;
             }
-
-            Console.WriteLine("Created gradebook {0}.", name);
             
+            Console.WriteLine("Created gradebook {0}.", name);
+            GradeBookUserInterface.CommandLoop(gradeBook);
         }
 
         public static void LoadCommand(string command)
